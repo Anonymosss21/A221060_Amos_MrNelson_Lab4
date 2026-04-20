0c101dea455844b9a8204a1f0eafc91496a1343d
Anonymosss21 <developerwong699@gmail.com>
2026-04-21 00:09:15 +0800

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "A221060_Amos_MrNelson_Lab4"
+include(":app")
+ 
